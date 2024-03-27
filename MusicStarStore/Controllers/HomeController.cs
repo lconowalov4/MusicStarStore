42cc2a7dfa6e27f5e97d0096f99a8e37b3fce376
lconowalov4 <rw6lfe@gmail.com>
2024-03-28 01:19:16 +0300

    Added new files and folders, and so changed some files

--- MusicStarStore/Controllers/HomeController.cs
+++ MusicStarStore/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace MusicStarStore.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
